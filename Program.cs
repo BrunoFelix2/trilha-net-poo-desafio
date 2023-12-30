cf467090ba187a4ca2097931d476543edcbd0135
Bruno Felix <bruno.felix28@outlook.com>
2023-12-30 09:19:51 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone iphone = new Iphone(numero: "(11)948465661", modelo: "5.5", imei:"222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
+Smartphone nokia = new Nokia(numero: "(21)987885456", modelo: "12.1", imei: "2121", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
